0523c2c0a3da40f25142bb4e95af897c7d716713
front.igor.m@gmail.com < >
2023-12-30 08:38:31 +0500

    master

--- assets/scripts/app.card.js
+++ assets/scripts/app.card.js
@@ -18,10 +18,22 @@ class AppCard {
     bindEvents = () => {
         for (let index = 0; index < this.$container.length; index++) {
             const container = this.$container[index];
+            const tagsWrapper = container.querySelector('.card__tags-wrapper');
+            const tags = tagsWrapper.querySelectorAll('.tag');
             const tagButton = container.querySelector('.js-card-tags-handle');
+            let tagItems = [];
 
-            tagButton.addEventListener('click', this.handleClickTagButton);
+            if ( tags.length > 2 ) {
+                for (let indexTag = 0; indexTag < tags.length; indexTag++) {
+                    tagItems.push(tags[indexTag].innerHTML);
+                }
+            }
 
+            window.addEventListener('resize', () => this.handleWindowResize(tagsWrapper, tagItems));
+
+            tagButton.addEventListener('click', () => this.handleClickTagButton(tagsWrapper, tagItems));
+
+            this.tagsInit(tagsWrapper, tagItems);
             this.galleryInit(container);
         }
     };
@@ -53,12 +65,53 @@ class AppCard {
         });
     };
 
-    handleClickTagButton() {
-        const container = this.closest('.js-card-tags');
-        const wrapper = container.querySelector('.js-card-tags-wrapper');
+    tagsInit = (container, data) => {
+        this.handleWindowResize(container, data);
+    };
 
-        wrapper.style.maxHeight = '600px';
-        this.remove();
+    handleWindowResize = (container, data) => {
+        const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+        if ( windowWidth < 750 ) {
+            if ( !container.closest('.js-card-tags').classList.contains('show') ) {
+                const button = container.closest('.js-card-tags').querySelector('.js-card-tags-handle');
+                let tagCount = container.querySelectorAll('.tag');
+
+                container.closest('.js-card-tags').classList.add('active');
+
+                for (let index = 0; index < tagCount.length; index++) {
+                    tagCount[index].remove();
+                }
+
+                for (let index = 0; index < 2; index++) {
+                    container.insertAdjacentHTML(`beforeend`, '<div class="tag">'+data[index]+'</div>')
+                }
+
+                button.innerText = 'Показать еще ' + String(data.length - 2);
+            }
+        } else {
+            this.getTags(container, data);
+        }
+    };
+
+    handleClickTagButton = (container, data) => {
+        this.getTags(container, data);
+
+        container.closest('.js-card-tags').classList.add('show');
+    };
+
+    getTags = (container, data) => {
+        let tagCount = container.querySelectorAll('.tag');
+
+        container.closest('.js-card-tags').classList.remove('active');
+
+        for (let index = 0; index < tagCount.length; index++) {
+            tagCount[index].remove();
+        }
+
+        for (let index = 0; index < data.length; index++) {
+            container.insertAdjacentHTML(`beforeend`, '<div class="tag">'+data[index]+'</div>')
+        }
     };
 }
 
